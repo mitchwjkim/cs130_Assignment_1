46f45e1b1871d201e6b0139d686474546f2270bc
mitchwjkim <mitch.kim.wjune@gmail.com>
2018-02-02 16:25:49 -0800

    fixed sphere.cpp

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -21,33 +21,64 @@ bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
     
     double disc = b * b - 4 * a * c;
     
+    // if(disc > 0) {
+    //     Hit hit1, hit2;
+    //     if(disc == 0) {
+    //         double t = (-b / (2 * a));
+    //         if(t > 0) {
+    //             Hit m_hit = {this, t, true};
+    //             hits.push_back(m_hit);
+    //         }
+    //     }
+    //     else {
+    //         double t1, t2;
+    //         t1 = (-b - pow(disc, 0.5)) / (2 * a);
+    //         t2 = (-b + pow(disc, 0.5)) / (2 * a);
+            
+    //         if(t1 > 0) {
+    //             hit1 = {this, t1, false};
+    //             hits.push_back(hit1);
+    //         }
+    //         if(t2 > 0) {
+    //             hit2 = {this, t2, true};
+    //             hits.push_back(hit2);
+    //         }
+    //     }
+    //     return true;
+    // }
+
     if(disc > 0) {
         Hit hit1, hit2;
+        double t1, t2;
+        t1 = (-b - pow(disc, 0.5)) / (2 * a);
+        t2 = (-b + pow(disc, 0.5)) / (2 * a);
+        
+        if(t1 > 0) {
+            hit1 = {this, t1, false};
+            hits.push_back(hit1);
+        }
+        else {
+            hit1 = {this, 0, false};
+            hits.push_back(hit1);
+        }
+        
+        hit2 = {this, t2, true};
+        hits.push_back(hit2);
+        return true;
+    }
+    else {
         if(disc == 0) {
             double t = (-b / (2 * a));
             if(t > 0) {
                 Hit m_hit = {this, t, true};
                 hits.push_back(m_hit);
             }
+            return true;
         }
         else {
-            double t1, t2;
-            t1 = (-b - pow(disc, 0.5)) / (2 * a);
-            t2 = (-b + pow(disc, 0.5)) / (2 * a);
-            
-            if(t1 > 0) {
-                hit1 = {this, t1, false};
-                hits.push_back(hit1);
-            }
-            if(t2 > 0) {
-                hit2 = {this, t2, true};
-                hits.push_back(hit2);
-            }
+            return false;
         }
-        return true;
     }
-    
-    return false;
 }
 
 vec3 Sphere::Normal(const vec3& point) const
